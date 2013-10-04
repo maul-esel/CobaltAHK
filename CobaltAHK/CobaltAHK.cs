244359935fe7496e522454d629071416a659de6c
maul.esel <maul.esel@go4more.de>
2013-10-04 17:04:42 +0200

    add project files and metadata

---
+++ CobaltAHK/CobaltAHK.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace CobaltAHK
+{
+	public class CobaltAHK
+	{
+		public void Execute(string code)
+		{
+			Execute(new StringReader(code));
+		}
+
+		public void Execute(TextReader code)
+		{
+			var expressions = parser.Parse(code);
+			Console.WriteLine(String.Format("{0} expressions", expressions.Length));
+			foreach (var e in expressions) {
+				Console.WriteLine("\t" + e.ToString());
+			}
+
+#if EXECUTE
+			var scope = new ExpressionTree.Scope();
+			var et = new List<Expression>();
+			foreach (var e in expressions) {
+				et.Add(ExpressionTree.Generator.Generate(e, scope));
+			}
+
+			var lambda = Expression.Lambda<Func<object>>(Expression.Block(et));
+			var exec = lambda.Compile();
+			exec();
+#endif
+		}
+
+		private Parser parser = new Parser();
+	}
+}
+
