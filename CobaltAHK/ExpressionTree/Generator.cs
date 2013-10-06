a6130f5784a9c59cbf1d6db821dd03bc314369cb
maul.esel <maul.esel@go4more.de>
2013-10-06 13:28:51 +0200

    use FunctionScope class for function definitions

--- CobaltAHK/ExpressionTree/Generator.cs
+++ CobaltAHK/ExpressionTree/Generator.cs
@@ -65,7 +65,7 @@ namespace CobaltAHK.ExpressionTree
 
 		private static DLR.Expression GenerateFunctionDefinition(FunctionDefinitionExpression func, Scope scope, ScriptSettings settings)
 		{
-			var funcScope = new Scope();
+			var funcScope = new FunctionScope(scope);
 
 			var prms = new List<DLR.ParameterExpression>();
 			var types = new List<Type>(prms.Count + 1);
